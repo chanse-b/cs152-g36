0ac97566f18fb1e7f3cfecbd9bae571842e48739
Chanse <cbhakta@stanford.edu>
2023-05-31 16:16:02 -0700

    confusion matrix ready. awaiting data

--- DiscordBot/classify.py
+++ DiscordBot/classify.py
@@ -3,6 +3,11 @@ import os
 import json
 import unidecode as decode
 from deep_translator import GoogleTranslator as GoogleTranslate
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 token_path = 'tokens.json'
 if not os.path.isfile(token_path):
@@ -39,3 +44,24 @@ def analyzer(text_to_analyze):
     #print(response)
     return response['attributeScores']['THREAT']['spanScores'][0]['score']['value']
 
+def confusionMatrix():
+    # Read the Excel file into a pandas DataFrame
+    df = pd.read_excel('path_to_your_excel_file.xlsx')
+
+    # Assuming the first column is 'strings' and the second column is 'labels'
+    messages = df.iloc[:,0]
+    y_true = df.iloc[:, 1]  # Get the ground truth labels
+    y_pred = [1 if analyzer(message) > .5 else 0 for message in messages] # Get the predicted labels
+
+    # Build the confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Visualize the confusion matrix
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.show()
+
+confusionMatrix()
